4637fb8e241207ec38e5d52b699de41960d87163
Julien <julien@cornut.org>
2016-04-05 17:33:58 +0200

    OverlapGraphs

--- OverlapGraphs.py
+++ OverlapGraphs.py
@@ -1,17 +1,15 @@
-from Bio import SeqIO
+from pprint import pprint
 
-# Use biopython to parse fasta files
-seqs = SeqIO.parse(open("OverlapGraphsData.txt"),'fasta')
+labels = []
+seqs = []
 
-# Build a dict of labels with each end of the given seq
-# stored in a tuple as value.
-ends = {e.id:(str(e.seq)[  :3],\
-              str(e.seq)[-3: ]) for e in seqs}
+with open("OverlapGraphsData.txt") as data:
+    l = data.readline().rstrip()
+    if l.startswith('>'):
+        labels.append(l)
+    else:
+        seqs.append(l)
+                
+dict = dict(zip(labels,seqs))
 
-# Take every dict entry and print matching start-ends.
-# As every edge can be viewed as start-end, we are sure
-# that every edge is present.
-for mlabel,(mstart,mend) in ends.items():
-    [print(mlabel,label) for label,(start,end) in ends.items()\
-     if (mend==start) and (label!=mlabel)]
-    
+pprint(labels)
