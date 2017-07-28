136c4c2fb2fe881f92e451c35a51f4bce69cb7e7
jlncrnt <julien.cornut@unige.ch>
2017-07-28 19:20:20 +0200

    Try with random

--- src/main/scala/challenges/GenomeAssemblyasShortestSuperstring.scala
+++ src/main/scala/challenges/GenomeAssemblyasShortestSuperstring.scala
@@ -2,26 +2,40 @@ package challenges
 
 import helpers.FastaParser
 
+import scala.util.Random
+
 object GenomeAssemblyasShortestSuperstring extends Challenge("GenomeAssemblyasShortestSuperstring.txt") {
 
   val chunks = FastaParser.parseString(data).map(_._2)
 
-  private def overlaps(tu: (String,String)): Option[String] = {
+  println("Number of chunks : " + chunks.size)
+
+  val min = chunks.head.size / 2
+
+  var i = 0
+
+  private def overlaps(tu: (String,String)): Option[List[String]] = {
     val (a,b) = tu
-    lazy val o = a.tails.filter(_.length >= a.length/2).find(b.startsWith)
-    lazy val p = b.tails.filter(_.length >= b.length/2).find(a.startsWith)
-    if      (o.isDefined) Some(a.dropRight(o.get.length) + b)
-    else if (p.isDefined) Some(b.dropRight(p.get.length) + a)
-    else    None
+    lazy val o = a.tails.filter(_.length >= min).find(b.startsWith)
+    lazy val p = b.tails.filter(_.length >= min).find(a.startsWith)
+    if      (o.isDefined) {println("O is defined"); Some(List(a.dropRight(o.get.length) + b))}
+    else if (p.isDefined) {println("P is defined"); Some(List(b.dropRight(p.get.length) + a))}
+    else    Some(List(a,b))
   }
 
   @scala.annotation.tailrec
-  def loop(chunks: List[String]): String = {
+  def loop(chks: List[String]): String = {
+    val chunks = Random.shuffle(chks)
+    //println(chunks)
     if(chunks.length < 2) chunks.head else {
-      println("Getting combs")
-      val combs = chunks.combinations(2) map { case List(a, b) => (a, b) }
-      println("Loop")
-      loop(combs.toList.par.flatMap(overlaps).toList)
+      //println("Getting combs")
+      var combs0 = if(chunks.length % 2 == 1) chunks.last :: chunks else chunks
+      println("Chunks left : " + combs0.length)
+      val combs = {combs0.sliding(2,2) map { case List(a, b) => (a, b) }}.toList
+      //println("Loop with combs size : " + combs.size)
+      //println(combs)
+
+      loop(combs.par.flatMap(overlaps).toList.flatten)
     }
   }
 
